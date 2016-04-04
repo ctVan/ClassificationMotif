c87818638a118096c8730ecaa586556caa76ed5a
ctVan <chuthanhvan95@gmail.com>
2016-04-04 21:19:05 +0700

    update discord finding

---
+++ DiscordFinder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindingMotifDiscord
+{
+    public abstract class AbstractDiscordFinder
+    {
+        protected int slidingWindow;
+        protected float[] data;
+        protected AbstractDistanceFunction distFunc;
+
+        public AbstractDiscordFinder(float[] data, int slidingWindow)
+        {
+            this.data = data;
+            this.slidingWindow = slidingWindow;
+            distFunc = null;
+        }
+
+        public abstract void findDiscord(out int discordLoc, out float largestDistance);
+    }
+
+    public class DiscordFinder : AbstractDiscordFinder
+    {
+        public DiscordFinder(float[] data, int slidingWindow)
+            : base(data, slidingWindow)
+        {
+            distFunc = new ImprovedEucleanDistance(data, slidingWindow);
+        }
+
+        public override void findDiscord(out int discordLoc, out float largestDistance)
+        {
+            discordLoc = -1;
+            largestDistance = 0;
+            float nearest_nei_dis = float.MaxValue;
+
+            for (int i = 0; i < data.Length - slidingWindow; i++)
+            {
+                for (int j = 0; j < data.Length - slidingWindow; j++)
+                {
+                    if (Math.Abs(i - j) >= slidingWindow)
+                    {
+                        if (distFunc.distance(i, j) < nearest_nei_dis)
+                        {
+                            nearest_nei_dis = distFunc.distance(i, j);
+                        }
+                    }
+                }
+                if (nearest_nei_dis > largestDistance)
+                {
+                    largestDistance = nearest_nei_dis;
+                    discordLoc = i;
+                }
+            }
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,38 +1,107 @@
 ﻿using System;
+using System.IO;
 
 namespace FindingMotifDiscord
 {
 	class MainClass
 	{
-		public static void Main (string[] args)
+        // this file path uses in debug mode
+        static string discordFolder = "..\\..\\data\\discord";
+        static string motifFolder = "..\\..\\data\\motif";
+        static string getDiscordFileName()
+        {
+            string[] fileNames = Directory.GetFiles(discordFolder);
+            int i = 1;
+            
+            foreach (string name in fileNames)
+            {
+                string n = name.Replace(discordFolder + "\\", "");
+                System.Console.Write(i.ToString()+". ");
+                System.Console.WriteLine(n);
+                i++;
+            }
+            System.Console.WriteLine("\nSelect file to compute:");
+            string idx = System.Console.ReadLine();
+
+            return fileNames[Int32.Parse(idx) - 1];
+        }
+        static string getMotifFileName()
+        {
+            string[] fileNames = Directory.GetFiles(motifFolder);
+            int i = 1;
+            foreach (string name in fileNames)
+            {
+                string n = name.Replace(discordFolder + "\\", "");
+                System.Console.Write(i.ToString()+". ");
+                System.Console.WriteLine(n);
+                i++;
+            }
+            System.Console.WriteLine("\nSelect file to compute:");
+            string idx = System.Console.ReadLine();
+
+
+            return fileNames[Int32.Parse(idx) - 1];
+
+        }
+
+        public static void motifFinding(float[] data)
+        {
+            // passing data to motif finder
+            const int slidingWindow = 5;
+            const float R = 0.01f;
+            AbstractMotifFinder motifFinder = new MotifFinder(data, slidingWindow, R);
+            int motifLoc;
+            int[] motifMatches;
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            System.Console.WriteLine("\nBegin finding motif ...");
+            motifFinder.findMotif(out motifLoc, out motifMatches);
+            System.Console.WriteLine("Motif finding finish");
+            watch.Stop();
+
+            System.Console.WriteLine("\nFound motif at location: " + motifLoc.ToString());
+            System.Console.WriteLine("Motif matches : ");
+            foreach (int motif in motifMatches)
+            {
+                System.Console.WriteLine(motif.ToString());
+            }
+
+            System.Console.WriteLine("Time to find motif : " + watch.ElapsedMilliseconds.ToString());
+        }
+        
+        public static void discordFinding(float [] data)
+        {
+            // passing data to motif finder
+            const int slidingWindow = 5;
+            AbstractDiscordFinder motifFinder = new DiscordFinder(data, slidingWindow);
+            int discordLoc;
+            float largestDis;
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            System.Console.WriteLine("\nBegin finding discord ...");
+            motifFinder.findDiscord(out discordLoc, out largestDis);
+            System.Console.WriteLine("Discord finding finish");
+            watch.Stop();
+
+            System.Console.WriteLine("\nFound discord at location: " + discordLoc.ToString());
+            System.Console.WriteLine("Largest distance: " + largestDis.ToString());
+
+            System.Console.WriteLine("Time to find discord : " + watch.ElapsedMilliseconds.ToString());
+        }
+        public static void Main (string[] args)
 		{
-			// reading data
-			IDataLoader dataLoader = new DataLoader ();
+            
+            // reading data
+            IDataLoader dataLoader = new DataLoader ();
 			string fileName;
-			System.Console.Write ("Input data file : ");
-			fileName = System.Console.ReadLine ();
-			float[] data = dataLoader.readFile (fileName);
+            fileName = getDiscordFileName();
 
-			// passing data to motif finder
-			const int slidingWindow = 5;
-			const float R = 0.01f;
-			AbstractMotifFinder motifFinder = new MotifFinder(data, 5, R);
-			int motifLoc;
-			int[] motifMatches;
+            float[] data = dataLoader.readFile (fileName);
 
-			var watch = System.Diagnostics.Stopwatch.StartNew ();
-			System.Console.WriteLine ("\nBegin finding motif ...");
-			motifFinder.findMotif (out motifLoc, out motifMatches);
-			System.Console.WriteLine ("Motif finding finish");
-			watch.Stop ();
+ //           motifFinding(data);
 
-			System.Console.WriteLine ("\nFound motif at location: " + motifLoc.ToString ());
-			System.Console.WriteLine ("Motif matches : ");
-			foreach (int motif in motifMatches) {
-				System.Console.WriteLine (motif.ToString ());
-			}
-
-			System.Console.WriteLine ("Time to find motif : " + watch.ElapsedMilliseconds.ToString ());
+            discordFinding(data);
+            System.Console.ReadKey();
 		}
 	}
 }
