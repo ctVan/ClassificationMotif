e05ec5b53a4c39f20d10328edd3980f1d04647be
beekill95 <nnn_bikiu0707@yahoo.com>
2016-08-06 15:20:19 +0700

    Introduce WidgetsLibrary to store new widgets

---
+++ WidgetsLibrary/TestWidget.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace WidgetsLibrary
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class TestWidget : Gtk.Bin
+	{
+		public TestWidget ()
+		{
+			this.Build ();
+		}
+	}
+}
+
