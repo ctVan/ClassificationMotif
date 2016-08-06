7010e0ea7adf4036e3999a738489c87f9335ac63
beekill95 <nnn_bikiu0707@yahoo.com>
2016-08-06 20:58:36 +0700

    Add HorizontalWidgetList

---
+++ Libraries/WidgetsLibrary/HorizontalWidgetsList.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace WidgetsLibrary
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class HorizontalWidgetsList : Gtk.Bin
+	{
+		public HorizontalWidgetsList ()
+		{
+			this.Build ();
+		}
+	}
+}
+
--- Tools/GraphTool/gtk-gui/GraphTool.GraphWindow.cs
+++ Tools/GraphTool/gtk-gui/GraphTool.GraphWindow.cs
@@ -7,6 +7,7 @@ namespace GraphTool
 		private global::Gtk.HBox hbox2;
 		private global::Gtk.VBox vbox4;
 		private global::Florence.GtkSharp.PlotWidget timeSeriresPlot;
+		private global::WidgetsLibrary.HorizontalWidgetsList horizontalwidgetslist1;
 		private global::Gtk.VBox vbox3;
 		private global::Gtk.CheckButton comparisonModeCheckBox;
 		private global::Gtk.ScrolledWindow GtkScrolledWindow;
@@ -33,17 +34,24 @@ namespace GraphTool
 //			this.timeSeriresPlot.WidthRequest = 678;
 //			this.timeSeriresPlot.HeightRequest = 405;
 //			this.timeSeriresPlot.Name = "timeSeriresPlot";
-			this.vbox4.Add (this.timeSeriresPlot);
+//			this.vbox4.Add (this.timeSeriresPlot);
 			global::Gtk.Box.BoxChild w1 = ((global::Gtk.Box.BoxChild)(this.vbox4 [this.timeSeriresPlot]));
 			w1.Position = 0;
 			w1.Expand = false;
 			w1.Fill = false;
+			// Container child vbox4.Gtk.Box+BoxChild
+			this.horizontalwidgetslist1 = new global::WidgetsLibrary.HorizontalWidgetsList ();
+			this.horizontalwidgetslist1.Events = ((global::Gdk.EventMask)(256));
+			this.horizontalwidgetslist1.Name = "horizontalwidgetslist1";
+			this.vbox4.Add (this.horizontalwidgetslist1);
+			global::Gtk.Box.BoxChild w2 = ((global::Gtk.Box.BoxChild)(this.vbox4 [this.horizontalwidgetslist1]));
+			w2.Position = 1;
 			this.hbox2.Add (this.vbox4);
-			global::Gtk.Box.BoxChild w2 = ((global::Gtk.Box.BoxChild)(this.hbox2 [this.vbox4]));
-			w2.Position = 0;
-			w2.Expand = false;
-			w2.Fill = false;
-			w2.Padding = ((uint)(4));
+			global::Gtk.Box.BoxChild w3 = ((global::Gtk.Box.BoxChild)(this.hbox2 [this.vbox4]));
+			w3.Position = 0;
+			w3.Expand = false;
+			w3.Fill = false;
+			w3.Padding = ((uint)(4));
 			// Container child hbox2.Gtk.Box+BoxChild
 			this.vbox3 = new global::Gtk.VBox ();
 			this.vbox3.Name = "vbox3";
@@ -56,10 +64,10 @@ namespace GraphTool
 			this.comparisonModeCheckBox.DrawIndicator = true;
 			this.comparisonModeCheckBox.UseUnderline = true;
 			this.vbox3.Add (this.comparisonModeCheckBox);
-			global::Gtk.Box.BoxChild w3 = ((global::Gtk.Box.BoxChild)(this.vbox3 [this.comparisonModeCheckBox]));
-			w3.Position = 0;
-			w3.Expand = false;
-			w3.Fill = false;
+			global::Gtk.Box.BoxChild w4 = ((global::Gtk.Box.BoxChild)(this.vbox3 [this.comparisonModeCheckBox]));
+			w4.Position = 0;
+			w4.Expand = false;
+			w4.Fill = false;
 			// Container child vbox3.Gtk.Box+BoxChild
 			this.GtkScrolledWindow = new global::Gtk.ScrolledWindow ();
 			this.GtkScrolledWindow.Name = "GtkScrolledWindow";
@@ -73,22 +81,22 @@ namespace GraphTool
 			this.timeSeriesTextView.CursorVisible = false;
 			this.GtkScrolledWindow.Add (this.timeSeriesTextView);
 			this.vbox3.Add (this.GtkScrolledWindow);
-			global::Gtk.Box.BoxChild w5 = ((global::Gtk.Box.BoxChild)(this.vbox3 [this.GtkScrolledWindow]));
-			w5.Position = 1;
+			global::Gtk.Box.BoxChild w6 = ((global::Gtk.Box.BoxChild)(this.vbox3 [this.GtkScrolledWindow]));
+			w6.Position = 1;
 			// Container child vbox3.Gtk.Box+BoxChild
 			this.infoLabel = new global::Gtk.Label ();
 			this.infoLabel.Name = "infoLabel";
 			this.infoLabel.LabelProp = global::Mono.Unix.Catalog.GetString ("Info Label");
 			this.vbox3.Add (this.infoLabel);
-			global::Gtk.Box.BoxChild w6 = ((global::Gtk.Box.BoxChild)(this.vbox3 [this.infoLabel]));
-			w6.Position = 2;
-			w6.Expand = false;
-			w6.Fill = false;
-			this.hbox2.Add (this.vbox3);
-			global::Gtk.Box.BoxChild w7 = ((global::Gtk.Box.BoxChild)(this.hbox2 [this.vbox3]));
-			w7.Position = 1;
+			global::Gtk.Box.BoxChild w7 = ((global::Gtk.Box.BoxChild)(this.vbox3 [this.infoLabel]));
+			w7.Position = 2;
 			w7.Expand = false;
 			w7.Fill = false;
+			this.hbox2.Add (this.vbox3);
+			global::Gtk.Box.BoxChild w8 = ((global::Gtk.Box.BoxChild)(this.hbox2 [this.vbox3]));
+			w8.Position = 1;
+			w8.Expand = false;
+			w8.Fill = false;
 			this.Add (this.hbox2);
 			if ((this.Child != null)) {
 				this.Child.ShowAll ();
